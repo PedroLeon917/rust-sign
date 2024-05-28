894f06e6e9767ac236f01a793b3ce64abdf60da5
PedroLeon917 <pedro.leon@everymundo.com>
2024-05-28 09:25:03 -0400

    Using crates bls_signatures, rand and serde

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,31 @@
+use bls_signatures::PrivateKey;
+use rand::thread_rng;
+use serde::{Deserialize, Serialize};
+
+#[derive(Deserialize, Serialize)]
+struct Transaction {
+    from: String,
+    to: String,
+    amount: i128,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let private = PrivateKey::generate(&mut thread_rng());
+    let public = private.public_key();
+    // println!("{private:?}, {public:?}");
+    
+    // let message = "Invest in Intel";
+
+    let message = Transaction {
+        amount: 100,
+        from: String::from("Leon"),
+        to: String::from("You"),
+    };
+
+    let serialize_message = serde_json::to_string(&message).unwrap();
+
+    let sig = private.sign(&serialize_message);
+
+    let verification = public.verify(sig, &serialize_message);
+    println!("Is the message authentic? {}", verification);
 }
